91ea33908e1199422142e0067a5655b596195ab4
Biswa Saraf <117895834+Biswa4414@users.noreply.github.com>
2024-01-06 23:10:05 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Timer from './Timer';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Your React App</h1>
+      <Timer />
+    </div>
+  );
+};
+
+export default App;
